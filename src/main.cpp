0b5476c43f5cb203c8ca132da8df1a08ba128434
AzureAD\NikolajGarroThroksø <nikolaj@decuria.dk>
2023-11-16 11:47:36 +0100

    move

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+/** \file */ 
+
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 #include <OneWire.h>
@@ -9,23 +11,31 @@
 #include "LittleFS.h"
 #include <Arduino_JSON.h>
 
-const char* ssid = "The_internet";
-const char* password = "Hm4p5m59";
+const char* ssid = "The_internet"; /**< WiFi SSID */
+const char* password = "Hm4p5m59"; /**< WiFi password */
 
-const int oneWireBus = 4;     
-OneWire oneWire(oneWireBus);
-String latestItmes;
+const int oneWireBus = 4; /**< GPIO pin for the OneWire bus */
+OneWire oneWire(oneWireBus); /**< OneWire object for temperature sensor */
 
-DallasTemperature sensors(&oneWire);
+String latestItmes; /**< String to store the latest temperature readings */
 
-AsyncWebServer server(80);
+DallasTemperature sensors(&oneWire); /**< Dallas Temperature sensor object */
 
-AsyncWebSocket ws("/ws");
-JSONVar readings;
-unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
+AsyncWebServer server(80); /**< AsyncWebServer instance */
+
+AsyncWebSocket ws("/ws"); /**< AsyncWebSocket instance for WebSocket communication */
+
+JSONVar readings; /**< JSON object to store temperature readings */
+
+unsigned long lastTime = 0; /**< Timestamp for last data notification */
+unsigned long timerDelay = 30000; /**< Delay for data notification (in milliseconds) */
 
 
+/**
+ * @brief Read temperature from the sensor.
+ * @param var The variable to read, either "TEMPC" or "TEMPF".
+ * @return A string representing the temperature in Celsius or Fahrenheit.
+ */
 String read_temp(const String& var) {
   sensors.requestTemperatures(); 
   if(var == "TEMPC")
@@ -37,10 +47,25 @@ String read_temp(const String& var) {
   }
 }
 
+/**
+ * @brief Notify all WebSocket clients with sensor readings.
+ * @param sensorReadings The sensor readings to send to clients.
+ */
 void notifyClients(String sensorReadings) {
   ws.textAll(sensorReadings);
 }
 
+/**
+ * @brief Handles incoming WebSocket messages and responds with historical data.
+ *
+ * This function processes WebSocket messages received from clients. It validates
+ * and handles incoming messages by sending the 'latestItmes' (historical temperature data)
+ * as a WebSocket response to clients.
+ *
+ * @param arg   Pointer to WebSocket frame information.
+ * @param data  Pointer to the received message data.
+ * @param len   Length of the received message data.
+ */
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
@@ -48,6 +73,20 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   }
 }
 
+/**
+ * @brief Handles WebSocket server events, such as client connections and disconnections.
+ *
+ * This function serves as an event handler for WebSocket server events. It logs
+ * client connections and disconnections, processes data events by delegating
+ * message handling to 'handleWebSocketMessage()', and handles other WebSocket-related events.
+ *
+ * @param server  Pointer to the WebSocket server instance.
+ * @param client  Pointer to the WebSocket client instance.
+ * @param type    Type of WebSocket event (e.g., WS_EVT_CONNECT, WS_EVT_DISCONNECT).
+ * @param arg     Argument data related to the event.
+ * @param data    Pointer to the event data.
+ * @param len     Length of the event data.
+ */
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
   switch (type) {
     case WS_EVT_CONNECT:
@@ -65,11 +104,18 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
   }
 }
 
+/**
+ * @brief Initialize WebSocket communication.
+ */
 void initWebSocket() {
   ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
 
+/**
+ * @brief Log temperature readings to a file.
+ * @param temperature The temperature reading to log.
+ */
 void logTemperature(float temperature) {
     File file = SD.open("/temperature_log.txt", FILE_APPEND);
     if (file) {
@@ -81,7 +127,13 @@ void logTemperature(float temperature) {
 }
 
 
-
+/**
+ * @brief Synchronizes historical temperature data for serving to clients.
+ *
+ * This function reads historical temperature data from the SD card and synchronizes
+ * it with the 'latestItmes' variable. It ensures that the number of historical
+ * readings does not exceed the maximum limit defined by 'maxItems'.
+ */
 void syncHistoricalData() {
   String data = "";
 
@@ -121,7 +173,9 @@ void syncHistoricalData() {
   latestItmes = data;
 }
 
-
+/**
+ * @brief Initialize SPIFFS (SPI Flash File System).
+ */
 void initSPIFFS() {
   if (!SPIFFS.begin(true)) {
     Serial.println("An error has occurred while mounting SPIFFS");
@@ -130,10 +184,23 @@ void initSPIFFS() {
 }
 
 
+/**
+ * @brief Initializes the ESP32 and sets up required components.
+ *
+ * This function performs the following tasks:
+ * 1. Initializes serial communication for debugging.
+ * 2. Initializes the Dallas Temperature sensor and OneWire communication.
+ * 3. Sets up the SPIFFS file system for serving web content and configuration files.
+ * 4. Initializes a WebSocket server for real-time communication.
+ * 5. Establishes a Wi-Fi connection with the specified credentials.
+ * 6. Initializes SD card communication for data storage.
+ * 7. Sets up HTTP server endpoints for handling requests.
+ */
 void setup(){
   Serial.begin(115200);
   sensors.begin();
   initSPIFFS();
+  initWebSocket();
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -176,7 +243,6 @@ void loop(){
 
   if ((millis() - lastTime) > timerDelay) {
     String sensorReadings = latestItmes;
-    Serial.print(sensorReadings);
     notifyClients(sensorReadings);
     lastTime = millis();
   }
